38aa7c207de7da059f5e6c395db37900a11122c3
BradenMccoy <bradyt.mccoy@gmail.com>
2022-05-10 14:54:12 -0700

    added preliminary apriori implementation

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import pandas as pd
 import numpy as np
 import sklearn as skl
+from apyori import apriori
 
 def main():
-    drugsComDB = pd.read_csv('drugsCom_raw/drugsComTrain_raw.tsv', sep='\t')
-    print(drugsComDB)
+    data = pd.read_csv('drug_consumption.data')
+    drugs = []
+    for i in range(0, data.shape[0]):
+        drugs.append([str(data.values[i, j]) for j in range(0, 32)])
+    print(drugs[0])
+
+    rules = apriori(drugs, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+    results = list(rules)
+
+    results = pd.DataFrame(results)
+    print(results.head())
 
 if __name__ == "__main__":
     main()
