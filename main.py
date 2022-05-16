189ffca8706aed91a97ddf490b3f956c0da187da
BradenMccoy <bradyt.mccoy@gmail.com>
2022-05-16 14:10:55 -0700

    fixed bug with reading, added column names, implementation without personality types

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ import sklearn as skl
 from apyori import apriori
 
 def main():
-    data = pd.read_csv('drug_consumption.data')
-    #data.drop(data.columns[[0]], axis=1, inplace=True)
-    #data.drop(data.columns[[6]], axis=1, inplace=True)
-    #data.drop(data.columns[[7]], axis=1, inplace=True)
-    #data.drop(data.columns[[8]], axis=1, inplace=True)
-    #data.drop(data.columns[[9]], axis=1, inplace=True)
-    #data.drop(data.columns[[10]], axis=1, inplace=True)
-    #data.drop(data.columns[[11]], axis=1, inplace=True)
-    #data.drop(data.columns[[12]], axis=1, inplace=True)
-    #drugs = []
-    #for i in range(0, data.shape[0]):
-        #drugs.append([str(data.values[i, j]) for j in range(0, 24)])
+    data = pd.read_csv('drug_consumption.data', header=None)
+    data.columns = ['Id', 'Age', 'Gender', 'Education', 'Country', 'Ethnicity', 'Nscore', 'Escore', 'Oscore', 'Ascore', 'Cscore', 'Impulsive', 'SS', 'Alcohol', 'Amphet', 'Amyl', 'Benzos', 'Caff', 'Cannabis', 'Choc', 'Coke', 'Crack', 'Ecstasy', 'Heroin', 'Ketamine', 'Legalh', 'LSD', 'Meth', 'Mushrooms', 'Nicotine', 'Semer', 'VSA']
+    data.drop('Id', axis=1, inplace=True)
+    data.drop('Nscore', axis=1, inplace=True)
+    data.drop('Escore', axis=1, inplace=True)
+    data.drop('Oscore', axis=1, inplace=True)
+    data.drop('Ascore', axis=1, inplace=True)
+    data.drop('Cscore', axis=1, inplace=True)
+    data.drop('Impulsive', axis=1, inplace=True)
+    data.drop('SS', axis=1, inplace=True)
+    drugs = []
+    for i in range(0, data.shape[0]):
+        drugs.append([str(data.values[i, j]) for j in range(0, 24)])
     print(drugs[0])
 
     rules = apriori(drugs, min_support = 0.05, min_confidence = 0.2, min_lift = 3, min_length = 2)
