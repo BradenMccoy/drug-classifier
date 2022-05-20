62e213c96a81ba5d7a0f4ab263aea70bbc9e4fa6
BradenMccoy <bradyt.mccoy@gmail.com>
2022-05-20 13:41:57 -0700

    removed old dbs

--- main.py
+++ main.py
@@ -11,33 +11,50 @@ def main():
     data.drop('Id', axis=1, inplace=True)
 
     # Legal Drugs
-    data.drop('Choc', axis=1, inplace=True)
     data.drop('Caff', axis=1, inplace=True)
     data.drop('Alcohol', axis=1, inplace=True)
+    data.drop('Cannabis', axis=1, inplace=True)
     data.drop('Legalh', axis=1, inplace=True)
-
+    data.drop('Nicotine', axis=1, inplace=True)
+    
+    # Illegal Drugs
+    # data.drop('Coke', axis=1, inplace=True)
+    # data.drop('Amphet', axis=1, inplace=True)
+    # data.drop('Amyl', axis=1, inplace=True)
+    # data.drop('Benzos', axis=1, inplace=True)
+    # data.drop('Crack', axis=1, inplace=True)
+    # data.drop('Ecstasy', axis=1, inplace=True)
+    # data.drop('Heroin', axis=1, inplace=True)
+    # data.drop('Ketamine', axis=1, inplace=True)
+    # data.drop('LSD', axis=1, inplace=True)
+    # data.drop('Meth', axis=1, inplace=True)
+    # data.drop('Mushrooms', axis=1, inplace=True)
+    # data.drop('Semer', axis=1, inplace=True)
+    # data.drop('VSA', axis=1, inplace=True)
 
     # Personality Traits
-    data.drop('Nscore', axis=1, inplace=True)
-    data.drop('Escore', axis=1, inplace=True)
-    data.drop('Oscore', axis=1, inplace=True)
-    data.drop('Ascore', axis=1, inplace=True)
-    data.drop('Cscore', axis=1, inplace=True)
-    data.drop('Impulsive', axis=1, inplace=True)
-    data.drop('SS', axis=1, inplace=True)
+    # data.drop('Nscore', axis=1, inplace=True)
+    # data.drop('Escore', axis=1, inplace=True)
+    # data.drop('Oscore', axis=1, inplace=True)
+    # data.drop('Ascore', axis=1, inplace=True)
+    # data.drop('Cscore', axis=1, inplace=True)
+    # data.drop('Impulsive', axis=1, inplace=True)
+    # data.drop('SS', axis=1, inplace=True)
 
     #print(data.head)
     drugs = []
     for i in range(0, data.shape[0]):
-        drugs.append([str(data.values[i, j]) for j in range(0, 20)])
+        drugs.append([str(data.values[i, j]) for j in range(0, 6)])
     #print(drugs[0])
 
-    rules = apriori(drugs, min_support = 0.05, min_confidence = 0.8, min_lift = 1.5, min_length = 2, use_colnames = True)
+    rules = apriori(drugs, min_support = 0.01, min_confidence = 0.7, min_lift = 1.5, min_length = 2, use_colnames = True)
 
     results = list(rules)
 
     #results = pd.DataFrame(results)
-    print(results[0])
+    for rule in results:
+        print(rule)
+        print()
 
 if __name__ == "__main__":
     main()
