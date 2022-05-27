441e3ac69fe10dd7b399adf771ec5a23b5be1eb0
albertj6 <albertj6@wwu.edu>
2022-05-27 12:04:37 -0700

    Add illegal drugs classification and png, add modules to main.

--- classification-main.py
+++ classification-main.py
@@ -5,11 +5,12 @@ from sklearn.metrics import f1_score, accuracy_score # metric for evaluation of 
 from sklearn.tree import DecisionTreeClassifier # decision tree 
 from sklearn.model_selection import train_test_split # split into train and test set
 from sklearn.tree import export_graphviz
-#from sklearn.externals.six import StringIO  
+from sklearn.externals.six import StringIO  
 from IPython.display import Image  
-#simport pydotplus
+import pydotplus
 from sklearn.tree import _tree
 from itertools import combinations
+
 
 def main():
     train = pd.read_csv('drug_consumption.data', header=None)
@@ -20,7 +21,73 @@ def main():
     # columns we are interested in
     feature_cols = ['Age', 'Gender', 'Education', 'Country', 'Ethnicity', 'Nscore', 'Escore', 'Oscore', 'Ascore', 'Cscore',
                     'Impulsive']
+    
+    # y is the output, the class we want to predict, and x contains the other columns of the dataset
+    legal_drugs = ['Alcohol', 'Caff', 'Cannabis', 'Choc', 'Legalh', 'Nicotine', 'Mushrooms']
+    illegal_drugs = ['SS', 'Amphet', 'Amyl', 'Benzos', 'Coke', 'Crack', 'Ecstasy',
+                     'Heroin', 'Ketamine', 'LSD', 'Meth', 'Mushrooms', 'Semer', 'VSA']
+    
+    # change to a binary classification, used or never used
+    for drug in legal_drugs:
+        train[drug] = train[drug].apply(mod_classification)
+    for drug in illegal_drugs:
+        train[drug] = train[drug].apply(mod_classification)
+    
+    combo_list = generate_combo_list(feature_cols) #all possible combinations of feature columns
 
+    # get file for legal drugs accuracy stats and rules
+    generate_tree(legal_drugs, feature_cols, combo_list, "legal", train)
+
+    # ditto for illegal drugs
+    generate_tree(illegal_drugs, feature_cols, combo_list, "illegal", train)
+
+# given a list of drugs and feature_columns, generate the trees for the possible
+# permutations of rules and find the highest accuracy score
+def generate_tree(drug_list, feature_columns, combo_list, filename, train):
+    print("------ Beginning Training sequence ------")
+    outfile = open(filename + ".txt", "w")
+    for drug in drug_list:
+        y = train[drug]
+
+        outfile.write("Predicting for " + drug + ":\n")
+        
+        # track the maximum accuracy rule for each drug
+        max_accuracy = 0
+        max_feature_list = []
+        max_DT = DecisionTreeClassifier(criterion="entropy", max_depth=4) # depth is 4
+        
+        # go through all possible attribute lists
+        for combo in combo_list:
+            # generate the tree
+            X = train[list(combo)]
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=2)
+            DT = DecisionTreeClassifier(criterion="entropy", max_depth=4) # depth is 4
+            DT.fit(X_train,y_train)
+            pred=DT.predict(X_test)
+            
+            outfile.write("\tUsing feature columns: ")
+            
+            for item in list(combo):
+                outfile.write(item + " ")
+            outfile.write("Accuracy: " + str(accuracy_score(y_test, pred)) + "\n")
+
+            # check if this is a maximum accuracy
+            if(accuracy_score(y_test, pred) > max_accuracy):
+                max_accuracy = accuracy_score(y_test, pred)
+                max_feature_list = combo
+                max_DT = DT
+
+        outfile.write("Maximum accuracy: " + str(max_accuracy) + "\n\n")
+        
+        # create a png for the maximum accuracy rule
+        pngname = "depth4-" + drug + ".png"
+        print(pngname)
+        print(max_feature_list)
+        png(max_DT, max_feature_list, pngname)
+    
+    outfile.close()
+
+def generate_combo_list(feature_cols):
     combo_list = [] # contains all possible combinations
     combo_list.append(feature_cols[:1])
     combo_list.append(feature_cols[:2])
@@ -33,65 +100,7 @@ def main():
     combo_list.append(feature_cols[:9])
     combo_list.append(feature_cols[:10]) 
     combo_list.append(feature_cols)
-    
-    # print(combo_list)
-    
-    # y is the output, the class we want to predict, and x contains the other columns of the dataset
-    legal_drugs = ['Alcohol', 'Caff', 'Cannabis', 'Choc', 'Legalh', 'Nicotine', 'Mushrooms']
-    illegal_drugs = ['SS', 'Amphet', 'Amyl', 'Benzos', 'Coke', 'Crack', 'Ecstasy',
-                     'Heroin', 'Ketamine', 'LSD', 'Meth', 'Mushrooms', 'Semer', 'VSA']
-    
-    # change to a binary classification, used or never used
-    for drug in legal_drugs:
-        train[drug] = train[drug].apply(mod_classification)
-    
-    print("------ Beginning Training sequence ------")
-    outfile = open("output.txt", "w")
-    for drug in legal_drugs:
-        y = train[drug]
-        outfile.write("Predicting for " + drug + ":\n")
-        
-        # track the maximum accuracy rule for each drug
-        max_accuracy = 0
-        for combo in combo_list:
-            X = train[list(combo)]
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=2)
-            DT = DecisionTreeClassifier(criterion="entropy", max_depth=4) # depth is 
-            DT.fit(X_train,y_train)
-            pred=DT.predict(X_test)
-            outfile.write("\tUsing feature columns: ")
-            
-            for item in list(combo):
-                outfile.write(item + " ")
-            outfile.write("Accuracy: " + str(accuracy_score(y_test, pred)) + "\n")
-
-            # check if this is a maximum accuracy
-            if(accuracy_score(y_test, pred) > max_accuracy):
-                max_accuracy = accuracy_score(y_test, pred)
-
-        outfile.write("Maximum accuracy: " + str(max_accuracy) + "\n\n")
-    
-    outfile.close()
-
-    # y = train[drugname]
-    # X = train[feature_cols] # only include the columns that we want
-
-    # Now we split the dataset in train and test sets
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=2)   
-
-    # Use the function imported above and apply fit() on it
-    # DT = DecisionTreeClassifier(criterion="entropy", max_depth=4) # depth is 
-    # DT.fit(X_train,y_train)
-
-    # We use the predict() on the model to predict the output
-    # pred=DT.predict(X_test)
-    
-    # for classification we use accuracy and F1 score
-    # print(accuracy_score(y_test,pred))
-    # print(f1_score(y_test,pred,average='micro'))
-
-    # png(DT, feature_cols, "depth5-alc.png")
-    # tree_to_code(DT, feature_cols)
+    return combo_list
 
 def mod_classification(x):
     if (x == 'CL0'):
@@ -103,8 +112,7 @@ def png(DT, feature_cols, file_name):
     dot_data = StringIO()
     export_graphviz(DT, out_file=dot_data,  
                     filled=True, rounded=True,
-                    special_characters=True,feature_names = feature_cols,class_names=['Never Used', 'Used a Decade Ago', 'Used in Last Decade', 
-                    'Used in Last Year', 'Used in Last Month', 'Used in Last Week', 'Used in Last Day'])
+                    special_characters=True,feature_names = feature_cols,class_names=['Never Used', 'Used'])
     graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
     graph.write_png(file_name)
     Image(graph.create_png())
